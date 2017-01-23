6ce16ac215f5967f5406a94a64fe8284c7513f7f
galaxy-captain <goskkyer@gmail.com>
2017-01-23 17:28:47 +0800

    微服务器 TCP服务器

---
+++ app/src/main/java/org/galaxy/microserver/server/HeartbeatThread.java
@@ -0,0 +1,53 @@
+package org.galaxy.microserver.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by galaxy on 2017/1/23.
+ */
+
+public class HeartbeatThread extends Thread {
+
+    private MicroServer mServer;
+
+    private List<MicroConnection> connections = new ArrayList<>();
+
+    public HeartbeatThread(MicroServer server) {
+        this.mServer = server;
+    }
+
+    public void addConnection(MicroConnection connection) {
+        connections.add(connection);
+    }
+
+    public synchronized void removeConnection(MicroConnection connection) {
+        connections.remove(connection);
+    }
+
+    private void execute() {
+
+        int size = connections.size();
+
+        for (int i = 0; i < size; i++) {
+
+            MicroConnection connection = connections.get(i);
+
+            boolean isConnect = connection.testConnection();
+
+            if (!isConnect) removeConnection(connection);
+
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+        while (mServer != null && mServer.getState().isRunning()) {
+            execute();
+        }
+
+    }
+
+}
